7064505a94b56c4998a6e89438902531b4ede8dd
Juan Escalante <jcec007@gmail.com>
2021-04-19 18:16:13 -0500

    first commit

---
+++ Database/Seeders/IcheckinDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Icheckin\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class IcheckinDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
