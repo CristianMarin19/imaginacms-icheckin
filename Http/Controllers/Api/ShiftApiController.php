f01e5edc5ab29356beb1f72415953a033d97a059
Slim Arias Sanhez <slimarias@github.com>
2021-06-01 18:07:17 -0500

    +ADD: shift was checked in event

--- Http/Controllers/Api/ShiftApiController.php
+++ Http/Controllers/Api/ShiftApiController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Modules\Icheckin\Entities\Shift;
+use Modules\Icheckin\Events\ShiftWasCheckedIn;
 use Modules\Icheckin\Http\Requests\CreateShiftRequest;
 use Modules\Icheckin\Http\Requests\UpdateShiftRequest;
 use Modules\Icheckin\Repositories\ShiftRepository;
@@ -41,24 +42,24 @@ class ShiftApiController extends BaseApiController
       $params->filter->usersByDepartment = $this->getUsersByDepartment($params);
       //Request to Repository
       $dataEntity = $this->service->getItemsBy($params);
-      
+
       //Response
       $response = [
         "data" => ShiftTransformer::collection($dataEntity)
       ];
-      
+
       //If request pagination add meta-page
       $params->page ? $response["meta"] = ["page" => $this->pageTransformer($dataEntity)] : false;
     } catch (\Exception $e) {
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-    
+
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
-  
+
+
   /**
    * GET A ITEM
    *
@@ -70,26 +71,26 @@ class ShiftApiController extends BaseApiController
     try {
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-      
+
       //Request to Repository
       $dataEntity = $this->service->getItem($criteria, $params);
-      
+
       //Break if no found item
       if (!$dataEntity) throw new \Exception('Item not found', 404);
-      
+
       //Response
       $response = ["data" => $dataEntity ? new ShiftTransformer($dataEntity) :""];
-      
+
     } catch (\Exception $e) {
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-    
+
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
-  
+
+
   /**
    * CREATE A ITEM
    *
@@ -100,31 +101,31 @@ class ShiftApiController extends BaseApiController
   {
     \DB::beginTransaction();
     try {
-     
+
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-     
+
       //Get data
       $data = $request->input('attributes');
-  
+
       //Validate Request
       $this->validateRequestApi(new CreateShiftRequest((array)$data));
-      
+
       $checkedAt =  Carbon::now()->format("Y-m-d H:i:s");
-  
+
       // se asigna el checkin al formato fecha Y-m-d H:i:s
       $data["checkin_at"] = $checkedAt;
       $data["checkin_by"] = $params->user->id;
-      
-  
+
+
       //Create item
       $shift = $this->service->create($data);
-  
+
       //Response
       $response = ["data" => new ShiftTransformer($shift)];
 
-      
- 
+
+
       \DB::commit(); //Commit to Data Base
     } catch (\Exception $e) {
       \DB::rollback();//Rollback to Data Base
@@ -134,8 +135,8 @@ class ShiftApiController extends BaseApiController
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
-  
+
+
   /**
    * UPDATE ITEM
    *
@@ -149,22 +150,22 @@ class ShiftApiController extends BaseApiController
     try {
       //Get data
       $data = $request->input('attributes');
-      
+
       //Validate Request
       $this->validateRequestApi(new UpdateShiftRequest((array)$data));
-      
+
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-      
+
       if(!isset($params->permissions["icheckin.shifts.edit"]) || !$params->permissions["icheckin.shifts.edit"]){
         $aux = $data;
         $data = [];
         $data["options"] = $aux["options"];
       }
-      
+
       //Request to Repository
       $this->service->updateBy($criteria, $data, $params);
-      
+
       //Response
       $response = ["data" => 'Item Updated'];
       \DB::commit();//Commit to DataBase
@@ -173,11 +174,11 @@ class ShiftApiController extends BaseApiController
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-    
+
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
+
   /**
    * CREATE A ITEM
    *
@@ -188,33 +189,36 @@ class ShiftApiController extends BaseApiController
   {
     \DB::beginTransaction();
     try {
-      
+
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-      
+
       //Get data
       $data = $request->input('attributes');
-      
+
       //Validate Request
       $this->validateRequestApi(new CreateShiftRequest((array)$data));
-      
+
       $checkedAt = Carbon::now()->format("Y-m-d H:i:s");
-      
+
       // se asigna el checkin al formato fecha Y-m-d H:i:s
       $finalData["checkin_at"] = $checkedAt;
       $finalData["checkin_by"] = $params->user->id;
       $finalData["created_by"] = $params->user->id;
       $finalData["options"] = $data["options"];
       $finalData["geo_location"] = $data["geo_location"];
-      
+
       //Create item
       $shift = $this->service->create($finalData);
-      
+
+      //shift event
+      event(new ShiftWasCheckedIn($shift));
+
       //Response
       $response = ["data" => new ShiftTransformer($shift)];
-      
-      
-      
+
+
+
       \DB::commit(); //Commit to Data Base
     } catch (\Exception $e) {
       \DB::rollback();//Rollback to Data Base
@@ -224,9 +228,9 @@ class ShiftApiController extends BaseApiController
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
-  
-  
+
+
+
   /**
    * UPDATE ITEM
    *
@@ -240,38 +244,38 @@ class ShiftApiController extends BaseApiController
     try {
       //Get data
       $data = $request->input('attributes');
-     
-      
+
+
       //Validate Request
       $this->validateRequestApi(new UpdateShiftRequest((array)$data));
-      
+
       //Get Parameters from URL.
       $params = $this->getParamsRequest($request);
-      
+
        $checkoutAt = date("Y-m-d H:i:s");
-       
+
         $finalData["checkout_at"] = $checkoutAt;
         $finalData["checkout_by"] = $params->user->id;
         if(isset($data["options"]))
           $finalData["options"] = $data["options"];
       if(isset($data["geo_location"]))
         $finalData["geo_location"] = $data["geo_location"];
-  
+
       $shift = Shift::find($criteria);
       if (!$shift) throw new \Exception('Shift not found', 404);
-  
+
       $date1 = new \DateTime($checkoutAt);
       $date2 = new \DateTime($shift->checkin_at);
       $diff = $date1->diff($date2);
-  
+
       $finalData["period_elapsed"] = $total = ((($diff->y * 365.25 + $diff->m * 30 + $diff->d) * 24 + $diff->h) * 60 + $diff->i)*60 + $diff->s;
-      
+
       //Request to Repository
       $shift = $this->service->updateBy($criteria, $finalData, $params);
       //Break if no found item
-      
-      
-      
+
+
+
       //Response
       $response = ["data" =>  new ShiftTransformer($shift)];
       \DB::commit();//Commit to DataBase
@@ -280,12 +284,12 @@ class ShiftApiController extends BaseApiController
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-    
+
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
-  
+
+
   /**
    * DELETE A ITEM
    *
@@ -298,10 +302,10 @@ class ShiftApiController extends BaseApiController
     try {
       //Get params
       $params = $this->getParamsRequest($request);
-      
+
       //call Method delete
       $this->service->deleteBy($criteria, $params);
-      
+
       //Response
       $response = ["data" => ""];
       \DB::commit();//Commit to Data Base
@@ -310,10 +314,10 @@ class ShiftApiController extends BaseApiController
       $status = $this->getStatusError($e->getCode());
       $response = ["errors" => $e->getMessage()];
     }
-    
+
     //Return response
     return response()->json($response, $status ?? 200);
   }
-  
-  
+
+
 }
