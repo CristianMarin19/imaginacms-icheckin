005ab474d82146ec5de69e5c3412315495527168
Michael <msolano@imaginacolombia.com>
2022-02-03 12:36:50 -0500

    Add: CMS Translations

---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,14 @@
+<?php
+return [
+  "label"  => [
+    "source"  => 'Fuente | Fuentes'
+  ],
+  "message"  => [
+    "settingSources"  => 'Fuentes asignadas para administrar',
+    "settingRoles"  => 'Roles asignados para administrar',
+    "settingDepartments"  => 'Departamentos asignados para administrar',
+    "settingRelatedDepartments"  => 'Departamentos relacionados',
+    "settingLandingPage"  => 'Pagina de destino',
+    "settingProductsGroups"  => 'Groups de productos asignados para administrar',
+  ]
+];
