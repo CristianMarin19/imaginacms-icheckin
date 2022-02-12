4984f0df7d32d62005127521b6ebd05eb59325af
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:01 -0500

    +Update: cms Translation files

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,14 +1,32 @@
 <?php
+
 return [
-  "label"  => [
-    "source"  => 'Source | Sources'
+  "sidebar" => [
+    "pageHome" => "Home",
+    "checkin" => "checkin",
+    "periodElapsed" => "Period elapsed",
+    "clockedInAt" => "Clocked In At",
+    "clockedInBy" => "Clocked In By",
+    "clockedInOut" => "Clocked In Out",
+    "clockedOutAt" => "Clocked Out At",
+    "Shifts" => "Shifts",
+    "commet" => "Commet",
+    "updateShift" => "Update shift",
+    "locationAddress" => " Permission to obtain the location was denied...Please allow it",
+    "noLocation" => "No location available",
+    "geoLocationCheckin" => "Initial location",
+    "geoLocationCheckout" => "Final location"
   ],
-  "message"  => [
-    "settingSources"  => 'Assigned sources to manage',
-    "settingRoles"  => 'Assigned Roles to manage',
-    "settingDepartments"  => 'Assigned Departments to manage',
-    "settingRelatedDepartments"  => 'Related departments',
-    "settingLandingPage"  => 'Landing page',
-    "settingProductsGroups"  => 'Assigned products group to manage',
+  "label" => [
+    "source" => "Source | Sources"
+  ],
+  "message" => [
+    "settingSources" => "Assigned sources to manage",
+    "settingRoles" => "Assigned Roles to manage",
+    "settingDepartments" => "Assigned Departments to manage",
+    "settingRelatedDepartments" => "Related departments",
+    "settingLandingPage" => "Landing page",
+    "settingProductsGroups" => "Assigned products group to manage"
   ]
 ];
+
