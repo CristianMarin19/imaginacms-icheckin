91cd3f0c75420a8ef94c5c3705e71215e3e80bdb
Juan Escalante <jcec007@gmail.com>
2022-11-21 08:11:38 -0500

    +Add: ModuleTableSeeder

--- Database/Seeders/IcheckinDatabaseSeeder.php
+++ Database/Seeders/IcheckinDatabaseSeeder.php
@@ -16,6 +16,7 @@ class IcheckinDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(IcheckinModuleTableSeeder::class);
         // $this->call("OthersTableSeeder");
     }
 }
