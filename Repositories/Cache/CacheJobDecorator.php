4531c544b076b94dfd2bb9aa26acb95b281fd165
Cristian Marin <marin101998@gmail.com>
2024-04-11 15:12:27 -0500

    +update | v10.x 11-04-24 last version v8.x

--- Repositories/Cache/CacheJobDecorator.php
+++ Repositories/Cache/CacheJobDecorator.php
@@ -13,4 +13,32 @@ class CacheJobDecorator extends BaseCacheDecorator implements JobRepository
         $this->entityName = 'icheckin.jobs';
         $this->repository = $job;
     }
+
+  public function getItemsBy($params)
+  {
+    $this->clearCache();
+
+    return $this->repository->getItemsBy($params);
+  }
+
+  public function getItem($criteria, $params = false)
+  {
+    $this->clearCache();
+
+    return $this->repository->getItem($criteria, $params);
+  }
+
+  public function updateBy($criteria, $data, $params = false)
+  {
+    $this->clearCache();
+
+    return $this->repository->updateBy($criteria, $data, $params);
+  }
+
+  public function deleteBy($criteria, $params = false)
+  {
+    $this->clearCache();
+
+    return $this->repository->deleteBy($criteria, $params);
+  }
 }
